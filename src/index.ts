0a0fc621d9a2217a9d96d7de3aee0f9b59b38f47
Kyle McAlpine <k@kylejm.dev>
2021-11-28 13:40:49 +0000

    remove uneeded import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import Spotify from './spotify'
-import { refreshAccessToken } from './spotify/token'
 
 const port = process.env.PORT || 3000
 const app = express()
